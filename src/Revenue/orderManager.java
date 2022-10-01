0ad5b1596d9491f7899594f37b40b8dd273bfc7a
yu-ace <870941399@qq.com>
2022-10-01 16:41:56 +0800

    拆分main函数

---
+++ src/Revenue/orderManager.java
@@ -0,0 +1,100 @@
+package Revenue;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class orderManager {
+    static int[] orderDishIds =new int[50];
+    static int[] orderDishCounts = new int[50];
+    static int orderDishCount = 0;
+
+    static File menuOrderFile = new File(FileUtils.getUserDirectoryPath() + "/learn/restaurant/menuMoney.dat");
+
+
+    public static void viewTheDish(){
+        for(int i = 0; i < dishCount; i++){
+            System.out.println(dishNames[i]+"\t"+ dishPrices[i]);
+        }
+    }
+
+    /**
+     * 对顾客下单扩容
+     */
+    public static void enCapacityOrder(){
+        int[] newOrderDishIds = new int[orderDishIds.length + 50];
+        int[] newOrderDishCounts = new int[orderDishCounts.length + 50];
+        for(int i = 0;i < dishNames.length;i++){
+            newOrderDishIds[i] = orderDishIds[i];
+            newOrderDishCounts[i] = orderDishCounts[i];
+        }
+        orderDishIds = newOrderDishIds;
+        orderDishCounts = newOrderDishCounts;
+    }
+
+    /**
+     * 顾客下单的菜单金额
+     * @return 总金额
+     */
+    public static double total(){
+        double sum = 0;
+        for(int i = 0;i < orderDishCount;i++){
+            sum = sum + (dishPrices[orderDishIds[i]] * orderDishCounts[i]);
+        }
+        return sum;
+    }
+
+    /**
+     * 顾客点单列表
+     * @param number 菜品编号
+     * @param amount 菜品数量
+     */
+    public static void addList(int number,int amount){
+        if(orderDishCount > orderDishIds.length / 2){
+            enCapacityOrder();
+        }
+        orderDishIds[orderDishCount] = number;
+        orderDishCounts[orderDishCount] = amount;
+        orderDishCount++;
+    }
+
+
+
+    /**
+     * 顾客下单
+     */
+    public static void save(){
+        try {
+            for(int i = 0; i < orderDishCount; i++){
+                String danList = orderDishIds[i] + "\t" + orderDishCounts[i];
+                FileUtils.write(menuOrderFile,danList + "\n","utf8",true);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    /**
+     * 为管理员读取顾客的点单
+     */
+    public static void load(){
+        try {
+            String orderDishInformation =FileUtils.readFileToString(menuOrderFile,"utf8");
+            String[] menuOrderLines = orderDishInformation.split("\n");
+            for(int i = 0;i < menuOrderLines.length;i++){
+                String[] menuMessages = menuOrderLines[i].split("\t");
+                int number = Integer.parseInt(menuMessages[0]);
+                int amount = Integer.parseInt(menuMessages[1]);
+                addList(number,amount);
+            }
+        } catch (IOException e) {
+            //避免第一次启动程序时，因为系统中没有创建文件而报错的情况。
+        }
+    }
+
+
+
+}
